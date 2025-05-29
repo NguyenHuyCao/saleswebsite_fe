2dde2671401688304337fae26edac6dc08d99d0d
NguyenHuyCao <caohk001asd@gmail.com>
2025-05-29 17:01:37 +0700

    Base System store page

---
+++ src/app/(user)/system/page.tsx
@@ -0,0 +1,14 @@
+// import StoreCommitmentSection from "@/components/system/StoreCommitmentSection";
+import StoreHeroSection from "@/components/system/SystemStore";
+import { Container } from "@mui/material";
+
+const SystemPage = () => {
+  return (
+    <Container>
+      <StoreHeroSection />
+      {/* <StoreCommitmentSection /> */}
+    </Container>
+  );
+};
+
+export default SystemPage;
