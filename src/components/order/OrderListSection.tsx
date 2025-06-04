46e3e01922c0b2f0f2f3b36800d3c95675280b93
NguyenHuyCao <caohk001asd@gmail.com>
2025-06-05 03:15:06 +0700

    Fetch data in history order page

--- src/components/order/OrderListSection.tsx
+++ src/components/order/OrderListSection.tsx
@@ -9,68 +9,14 @@ import {
   Chip,
   Grid,
   Divider,
+  Stack,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import BuildCircleIcon from "@mui/icons-material/BuildCircle";
 import ShippingStatusChip from "./ShippingStatusChipProps";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-
-const orders = [
-  {
-    orderId: 5,
-    status: "PENDING",
-    shippingStatus: "LOCAL_SHIPPER",
-    shippingAddress: "456 Trần Hưng Đạo, Quy Nhơn",
-    paymentMethod: "COD",
-    shippingMethod: "LOCAL_SHIPPER",
-    totalAmount: 0,
-    createdAt: "2025-05-29T13:20:43.472Z",
-    items: [
-      {
-        productName: "Máy xay cỏ",
-        quantity: 2,
-        unitPrice: 0,
-      },
-    ],
-    paymentStatus: "PENDING",
-  },
-  {
-    orderId: 2,
-    status: "PENDING",
-    shippingStatus: "LOCAL_SHIPPER",
-    shippingAddress: "456 Trần Hưng Đạo, Quy Nhơn",
-    paymentMethod: "COD",
-    shippingMethod: "LOCAL_SHIPPER",
-    totalAmount: 0,
-    createdAt: "2025-05-29T13:20:43.472Z",
-    items: [
-      {
-        productName: "Máy xay cỏ",
-        quantity: 2,
-        unitPrice: 0,
-      },
-    ],
-    paymentStatus: "PENDING",
-  },
-  {
-    orderId: 1,
-    status: "PENDING",
-    shippingStatus: "LOCAL_SHIPPER",
-    shippingAddress: "456 Trần Hưng Đạo, Quy Nhơn",
-    paymentMethod: "COD",
-    shippingMethod: "LOCAL_SHIPPER",
-    totalAmount: 0,
-    createdAt: "2025-05-29T13:20:43.472Z",
-    items: [
-      {
-        productName: "Máy xay cỏ",
-        quantity: 2,
-        unitPrice: 0,
-      },
-    ],
-    paymentStatus: "PENDING",
-  },
-];
 
 const formatDate = (dateString: string) => {
   const date = new Date(dateString);
@@ -92,57 +38,85 @@ const statusColor = (status: string) => {
 
 const OrderListSection = () => {
   const router = useRouter();
+  const [orders, setOrders] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const token = localStorage.getItem("accessToken");
+      if (!token) return;
+
+      try {
+        const res = await fetch("http://localhost:8080/api/v1/history_orders", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const json = await res.json();
+        setOrders(json.data || []);
+      } catch (err) {
+        console.error("Lỗi khi lấy lịch sử đơn hàng:", err);
+      }
+    };
+
+    fetchOrders();
+  }, []);
 
   return (
     <Box mt={6}>
+      {orders.length === 0 && (
+        <Typography textAlign="center" mt={5}>
+          Bạn chưa có đơn hàng nào.
+        </Typography>
+      )}
       {orders.map((order) => (
         <Box key={order.orderId} mb={2}>
-          <Accordion sx={{ mb: 2 }}>
+          <Accordion>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Box sx={{ width: "100%" }}>
-                <Grid container spacing={2} alignItems="center">
-                  <Grid size={{ xs: 12, md: 3 }}>
-                    <Typography fontWeight={600}>
-                      Đơn hàng #{order.orderId}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      Ngày đặt: {formatDate(order.createdAt)}
-                    </Typography>
-                  </Grid>
+              <Grid container spacing={2} alignItems="center">
+                <Grid size={{ xs: 12, md: 3 }}>
+                  <Typography fontWeight={600}>
+                    Đơn hàng #{order.orderId}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Ngày đặt: {formatDate(order.createdAt)}
+                  </Typography>
+                </Grid>
 
-                  <Grid size={{ xs: 6, md: 2 }}>
-                    <Typography variant="body2">
-                      Tổng tiền:{" "}
-                      <strong>
-                        {order.totalAmount.toLocaleString("vi-VN")}₫
-                      </strong>
-                    </Typography>
-                  </Grid>
+                <Grid size={{ xs: 6, md: 2 }}>
+                  <Typography variant="body2">
+                    Tổng tiền:{" "}
+                    <strong>
+                      {order.totalAmount.toLocaleString("vi-VN")}₫
+                    </strong>
+                  </Typography>
+                </Grid>
 
-                  <Grid size={{ xs: 6, md: 3 }}>
-                    <Chip
-                      sx={{ width: 150 }}
-                      label={
-                        order.status === "PENDING"
-                          ? "Chờ xác nhận"
-                          : order.status === "COMPLETED"
-                          ? "Đã hoàn tất"
-                          : "Đã hủy"
-                      }
-                      color={statusColor(order.status)}
-                    />
-                  </Grid>
+                <Grid size={{ xs: 6, md: 3 }}>
+                  <Chip
+                    sx={{ width: 150 }}
+                    label={
+                      order.status === "PENDING"
+                        ? "Chờ xác nhận"
+                        : order.status === "COMPLETED"
+                        ? "Đã hoàn tất"
+                        : "Đã hủy"
+                    }
+                    color={statusColor(order.status)}
+                  />
+                </Grid>
 
-                  <Grid size={{ xs: 12, md: 2 }}>
-                    <ShippingStatusChip status={order.shippingStatus} />
-                  </Grid>
+                <Grid size={{ xs: 12, md: 2 }}>
+                  <ShippingStatusChip status={order.shippingMethod} />
+                </Grid>
 
-                  <Grid size={{ xs: 12, md: 2 }}>
+                <Grid size={{ xs: 12, md: 2 }}>
+                  <Stack direction="row" spacing={1}>
+                    {/* HỖ TRỢ */}
                     <Box
                       onClick={() => router.push("/contact")}
                       display="flex"
                       alignItems="center"
-                      maxWidth={100}
                       px={1.5}
                       py={0.5}
                       border="1px solid"
@@ -153,7 +127,6 @@ const OrderListSection = () => {
                       fontWeight={500}
                       sx={{
                         cursor: "pointer",
-                        textDecoration: "none !important",
                         transition: "all 0.2s",
                         "&:hover": {
                           backgroundColor: "error.main",
@@ -164,16 +137,42 @@ const OrderListSection = () => {
                       <HelpOutlineIcon sx={{ fontSize: 18, mr: 1 }} />
                       Hỗ trợ
                     </Box>
-                  </Grid>
+
+                    {/* BẢO HÀNH */}
+                    <Box
+                      onClick={() => router.push("/warranty")}
+                      display="flex"
+                      alignItems="center"
+                      px={1.5}
+                      py={0.5}
+                      border="1px solid"
+                      borderColor="info.main"
+                      color="info.main"
+                      borderRadius={1}
+                      fontSize={14}
+                      fontWeight={500}
+                      sx={{
+                        cursor: "pointer",
+                        transition: "all 0.2s",
+                        "&:hover": {
+                          backgroundColor: "info.main",
+                          color: "#fff",
+                        },
+                      }}
+                    >
+                      <BuildCircleIcon sx={{ fontSize: 18, mr: 1 }} />
+                      Bảo hành
+                    </Box>
+                  </Stack>
                 </Grid>
-              </Box>
+              </Grid>
             </AccordionSummary>
 
             <AccordionDetails>
               <Typography fontWeight={600} mb={1}>
                 Sản phẩm trong đơn:
               </Typography>
-              {order.items.map((item, i) => (
+              {order.items.map((item: any, i: number) => (
                 <Box
                   key={i}
                   display="flex"
@@ -196,7 +195,8 @@ const OrderListSection = () => {
                 <strong>Phương thức thanh toán:</strong> {order.paymentMethod}
               </Typography>
               <Typography>
-                <strong>Phương thức vận chuyển:</strong> {order.shippingMethod}
+                <strong>Phương thức vận chuyển:</strong>{" "}
+                {order.shippingMethod || "Không rõ"}
               </Typography>
             </AccordionDetails>
           </Accordion>
